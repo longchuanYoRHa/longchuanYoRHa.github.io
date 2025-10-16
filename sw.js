0b1fd87dd38052d4a0dd4535f5080b5a44ff2a00
MoxueF <MoxueF@users.noreply.github.com>
2025-10-16 09:01:29 +0000

    deploy: longchuanYoRHa/Personal_website@d0caf9ac46fc609bac53e3a2be3c057caa8bb01c

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1760605067161";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1760605288114";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
