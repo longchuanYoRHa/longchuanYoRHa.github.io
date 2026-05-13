9c8af5d806da07ec856563a655e112b08ade60cd
longchuanYoRHa <longchuanYoRHa@users.noreply.github.com>
2026-05-13 21:44:22 +0000

    deploy: longchuanYoRHa/Personal_website@755d93587af89c10d9a5f217166a45dab345ea86

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1778418032147";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1778708650817";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
